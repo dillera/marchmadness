7601774b3d4029c9e4e1da931e7b9ea20ac3655d
Andy Diller <dillera@dillernet.com>
2023-11-11 22:41:21 +0000

    display 10 messages not working, going to cut out any creation of auto records in app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -7,7 +7,7 @@ import os
 from sqlalchemy import inspect
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../marchmessages.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///marchmessages.db'
 app.config['SQLALCHEMY_BINDS'] = {'userdb': 'sqlite:///userdb.sqlite3'}
 app.config['SECRET_KEY'] = 'your-secret-key'
 
@@ -17,26 +17,34 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
+#################################################################
+
 class User(UserMixin, db.Model):
     __bind_key__ = 'userdb'
     __tablename__ = 'user'
     
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
+    id            = db.Column(db.Integer, primary_key=True)
+    username      = db.Column(db.String(80), unique=True, nullable=False)
     password_hash = db.Column(db.String(128))
     
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+##########
+
 class Message(db.Model):
     __tablename__ = 'messages'
     
-    message_id = db.Column(db.Text, nullable=False, index=True, unique=True)
+    id         = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    message_id = db.Column(db.Text)
     from_email = db.Column(db.Text)
-    date = db.Column(db.Text)
-    to_email = db.Column(db.Text)
-    subject = db.Column(db.Text)
-    content = db.Column(db.Text)
+    date       = db.Column(db.Text)
+    to_email   = db.Column(db.Text)
+    subject    = db.Column(db.Text)
+    content    = db.Column(db.Text)
+
+
+#################################################################
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -59,6 +67,26 @@ def init_user_db():
             print("Admin user created with username 'admin' and password 'admin'")
 
 
+def init_db():
+    """Create the database and add a sample record."""
+    db.create_all()  # Create the tables if they don't already exist
+    
+    # Check if there are any entries already (if not, this is a new database)
+    if not Message.query.first():
+        # Add a sample record
+        sample_message = Message(
+            message_id='sample_id',
+            from_email='example@example.com',
+            date='2023-01-01 00:00:00',
+            to_email='recipient@example.com',
+            subject='Sample Message',
+            content='This is a sample message content to confirm database creation.'
+        )
+        db.session.add(sample_message)
+        db.session.commit()
+        print("Added a sample message to the 'messages' table.")
+
+####################################################################################
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
@@ -70,16 +98,17 @@ def login():
             return redirect(url_for('search'))
     return render_template('login.html', form=form)
 
+####################################################################################
 @app.route('/search', methods=['GET', 'POST'])
 @login_required
 def search():
     form = SearchForm()
     messages = []
-    if form.validate_on_submit():
+    if request.method == 'POST' and form.validate_on_submit():
         search_query = form.search.data
         search_field = form.field.data
         match_type = form.match_type.data
-
+        
         # Determine the type of match and construct the filter
         if match_type == 'partial':
             search_filter = getattr(Message, search_field).like(f'%{search_query}%')
@@ -88,9 +117,14 @@ def search():
 
         # Apply the filter to the query
         messages = Message.query.filter(search_filter).all()
+    else:
+        # If it's a GET request, show the first 10 messages
+        print("Get request.")
+        messages = Message.query.order_by(Message.id).limit(10).all()
 
     return render_template('search.html', form=form, messages=messages)
 
+####################################################################################
 
 @app.route('/logout')
 @login_required
@@ -101,5 +135,8 @@ def logout():
 
 if __name__ == '__main__':
     with app.app_context():
-        init_user_db()  # Initialize the user database
-    app.run(debug=True)
+        init_user_db()  # Initialize the user database with admin user
+#        init_db()       # Initialize the message database with a sample record if necessary
+
+    app.run(debug=True,host = '0.0.0.0')
+
